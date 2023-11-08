da077b536452ed110e0716f75ae795b7be6450a4
fmruiz <x.francoruiz@gmail.com>
2023-11-08 09:40:10 -0300

    add big o notations notes

---
+++ big-o-notation.js
@@ -0,0 +1,16 @@
+/**
+ * BIG O NOTATION:
+ *
+ * Notes:
+ *
+ * 1) - It allows us to talk formally about how the runtime of an algorithm
+ * Grows as the input grow
+ *
+ * 2) - Using for measure the performance and complexity of the algorithms
+ *
+ * 3) - Using to describe how much time or resources consume one specific fn
+ *
+ * 4) - It is very important because the goal of the Big O Notation is to identify
+ * And compare between algorithms to discover who is more efficient
+ * If we talk about resources and execution time
+ */
